19bcf5f8609d53bf4bfbd8b02706c0013d3eea49
nitish.k.rai <nitish.k.rai@globallogic.com>
2016-04-27 09:12:49 +0530

    added elastic search dependency

---
+++ src/main/java/com/mrll/javelin/search/application/config/ElasticsearchConfiguration.java
@@ -0,0 +1,25 @@
+package com.mrll.javelin.search.application.config;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * @author nitish.k.rai
+ *
+ */
+@Configuration
+public class ElasticsearchConfiguration {
+	@Bean
+	public Client client() {
+		TransportClient client = new TransportClient();
+		TransportAddress address = new InetSocketTransportAddress("localhost", 9300);
+		client.addTransportAddress(address);
+		return client;
+	}
+
+}
